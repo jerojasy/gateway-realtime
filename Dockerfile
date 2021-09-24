cbba17c455ae94f58625b41e57c3c9b608b90e88
unknown <jaimee.rojas@hotmail.com>
2021-09-24 12:00:17 -0300

    se agrega gunicorn y servicio de mensajeria

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.8.3-alpine
 WORKDIR /src
 
 ENV FLASK_APP index.py
+ENV PYTHONUNBUFFERED 1
 
 COPY requirements.txt requirements.txt
 
@@ -11,4 +12,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-# CMD ["flask", "run"]
+CMD ["gunicorn","index:app","-b","0.0.0.0:8011","--log-level=debug","--workers","2", "--reload"]
--- src/index.py
+++ src/index.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
-from redis import Redis
+from flask import Flask, render_template, request, flash
+from flask.helpers import url_for
+# from redis import Redis
+import requests
+import json
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+# redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def index():
@@ -36,9 +41,51 @@ def campañas():
 def campañas_vista_empresa():
     return render_template('campañas_vista_empresa.html')
 
-@app.route('/test')
+@app.route('/mensajeria', methods=['GET','POST'])
+def mensajeria():
+
+    if request.method == 'POST':
+
+        url = "http://20.62.155.25:8008/api/msg/send"
+
+        payload = json.dumps({
+        "from": request.form['from'],
+        "to": request.form['to'],
+        "text": request.form['text'],
+        "dlr": False,
+        "dlr_url": "string",
+        "tag": "string",
+        "profile": "string",
+        "eta": "2021-09-23T20:10:04.309Z",
+        "eta_seconds": 0,
+        "retries": 0
+        })
+        headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(request.form)
+        print(response.text)
+        print(f"status:{response.status_code}")
+        if response.status_code == 200:
+            return render_template('mensajeria.html', msg = "Mensaje Enviado", flag = 'success')
+        else:
+            return render_template('mensajeria.html', msg = "No se ha podido envíar el mensaje", flag = 'danger')
+    return render_template('mensajeria.html', msg = "")
+
+@app.route('/configurar_operadora')
+def configurar_operadora():
+    return render_template('configurar_operadora.html')
+
+@app.route('/test', methods=['POST'])
 def test():
-    return render_template('test.html')
+    
+    return redirect(url_for('mensajeria'), msg = "Enviado")
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
